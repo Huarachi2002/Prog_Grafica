dcfff91f828ef27b7a4b9f8be51bd2c6f4a50173
Yeferson Huarachi <roberthuarachi27@gmail.com>
2024-09-01 23:41:18 -0400

    Agregar archivos de proyecto.

---
+++ TareaGrafica/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+using TareaGrafica;
+using OpenTK;
+
+namespace TareaGrafica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hola mundo");
+
+            using (Game game = new(500, 500))
+            {
+                game.Run();
+            }
+        }
+    }
+}
